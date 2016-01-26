2fa6f242d63bb3eea10d9d8e6c415768b6d225ec
Andy Stabler <andy_60@hotmail.co.uk>
2016-01-26 23:05:58 +0000

    Kill the config guff

--- config.rb
+++ config.rb
@@ -12,12 +12,6 @@ page '/*.txt', layout: false
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :css_dir, 'assets/stylesheets'
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
 
@@ -25,17 +19,6 @@ set :css_dir, 'assets/stylesheets'
 configure :development do
   activate :livereload
 end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 # Build-specific configuration
 configure :build do
