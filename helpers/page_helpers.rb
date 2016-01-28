8e3bf8da325f76bbfb7cc1ec86d00e1d24d11777
Andy Stabler <andy_60@hotmail.co.uk>
2016-01-28 22:54:23 +0000

    Extract blog link helpers into page_helpers.rb

--- helpers/page_helpers.rb
+++ helpers/page_helpers.rb
@@ -5,4 +5,14 @@ module PageHelpers
     title = [current_page.data.title, opts[:subsections], "Andy Stabler"].flatten.compact.join(" - ")
     content_tag :title, title
   end
+
+  def link_to_previous_post
+    article = previous_post
+    link_to(article.data.title, article.url) if !article.nil?
+  end
+
+  def link_to_next_post
+    article = next_post
+    link_to(article.data.title, article.url) if !article.nil?
+  end
 end
--- lib/blog.rb
+++ lib/blog.rb
@@ -7,14 +7,13 @@ class Blog < ::Middleman::Extension
   end
 
   helpers do
-    def link_to_previous_post
-      article = articles.select { |article| article.data.published_on < current_page.data.published_on }.last
-      link_to(article.data.title, article.url) if !article.nil?
+
+    def previous_post
+      articles.select { |article| article.data.published_on < current_page.data.published_on }.last
     end
 
-    def link_to_next_post
-      article = articles.select { |article| article.data.published_on > current_page.data.published_on }.first
-      link_to(article.data.title, article.url) if !article.nil?
+    def next_post
+      articles.select { |article| article.data.published_on > current_page.data.published_on }.first
     end
 
     def articles(&block)
