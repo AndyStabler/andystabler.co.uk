1933585c7dfbb2fa87e71813313edfc5fc8cf7f2
Andy Stabler <andy_60@hotmail.co.uk>
2016-02-13 18:14:28 +0000

    Deploy the current branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :application, 'andystabler.co.uk'
 set :repo_url, 'git@github.com:AndyStabler/andystabler.co.uk.git'
 set :rvm1_ruby_version, "ruby-2.3.0"
 set :deploy_via, :remote_cache
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 before 'deploy:updated', 'deploy:build'
 before 'deploy', 'rvm1:install:rvm'  # install/update RVM
