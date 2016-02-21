d8480c17802cb4dcf432ce8f31cb931f11a9bccb
Andy Stabler <andy_60@hotmail.co.uk>
2016-02-21 01:25:11 +0000

    Add meta tag helpers

--- helpers/page_helpers.rb
+++ helpers/page_helpers.rb
@@ -1,9 +1,12 @@
 module PageHelpers
 
-  def title_tag(opts = {})
+  def title_content
     # connect the title, subsections, and "Andy Stabler" to get a title like "A blog post - Blog - Andy Stabler"
-    title = [current_page.data.title, opts[:subsections], "Andy Stabler"].flatten.compact.join(" - ")
-    content_tag :title, title
+    [current_page.data.title, section, "Andy Stabler"].flatten.compact.join(" - ")
+  end
+
+  def title_tag(opts = {})
+    content_tag :title, title_content
   end
 
   # Public: Determine the section the current page belongs to. Right now
@@ -24,5 +27,41 @@ module PageHelpers
   def section
     pages = current_page.path.split("/")
     pages.first.capitalize if pages.count > 1
+  end
+
+  def social_meta_tags(options = {})
+    title = options[:title] || title_content
+    description = options[:description] || current_page.data.description
+    image = options[:image] || current_page.data.image
+    path = config.domain + current_page.url
+
+    standard_tags(title, description, image) +
+    og_tags(title, description, path, image) +
+    twitter_tags(title, description, image)
+  end
+
+  private
+
+  def standard_tags(title, description, image)
+    content_tag(:meta, nil, content: title, property: 'title') +
+    (description ? content_tag(:meta, nil, content: description, property: 'description') : '') +
+    (image ? content_tag(:link, nil, href: image, property: 'image_src') : '') +
+    content_tag(:meta, nil, content: title, itemprop: 'name') +
+    (description ? content_tag(:meta, nil, content: description, itemprop: 'description') : '') +
+    (image ? content_tag(:meta, nil, content: image, itemprop: 'image') : '')
+  end
+
+  def og_tags(title, description, path, image)
+    content_tag(:meta, nil, content: title, property: 'og:title') +
+    (description ? content_tag(:meta, nil, content: description, property: 'og:description') : '') +
+    content_tag(:meta, nil, content: path, property: 'og:url') +
+    (image ? content_tag(:meta, nil, content: image, property: 'og:image') : '')
+  end
+
+  def twitter_tags(title, description, image)
+    content_tag(:meta, nil, content: 'twitter summary', name: 'twitter:card') +
+    content_tag(:meta, nil, content: title, name: 'twitter:title') +
+    (description ? content_tag(:meta, nil, content: description, name: 'twitter:description') : '') +
+    (image ? content_tag(:meta, nil, content: image, name: 'twitter:image') : '')
   end
 end
